7399e6806517bf5f8d43912de8069c4f5ebf4163
Rolva Mashale <Rolva@zensar.local>
2025-08-13 11:58:34 +0200

    added test table

--- src/main/java/com/demoqa/pages/HomePage.java
+++ src/main/java/com/demoqa/pages/HomePage.java
@@ -2,16 +2,24 @@ package com.demoqa.pages;
 
 import com.base.BasePage;
 import com.demoqa.pages.FormsPage.FormsPage;
+import com.demoqa.pages.elements.ElementsPage;
 import org.openqa.selenium.By;
 
 import static utilities.JavaScriptUtility.scrollToElementJS;
 
 public class HomePage extends BasePage {
     private By formsCard = By.xpath("//div[@id='app']//h5[text()='Forms']");
+    private  By elementsCard = By.xpath("//div[@id='app']//h5[text()='Elements']");
 
     public FormsPage goToForms() {
         scrollToElementJS(formsCard);
         click(formsCard);
         return new FormsPage();
+    }
+
+    public ElementsPage goToElements() {
+        scrollToElementJS(elementsCard);
+        click(elementsCard);
+        return new ElementsPage();
     }
 }
